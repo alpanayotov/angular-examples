bd0a6fc8bed379d2e1fa684e4c1f2de10dac528e
alpanayotov <alexander.panayotov@gmail.com>
2016-07-15 17:12:51 +0300

    Simple TODO app

---
+++ js/app-todo.js
@@ -0,0 +1,67 @@
+( function() {
+	var app;
+	app = angular.module('toDos', []);
+
+	app.controller('listController', ['$http', function( $http ) {
+
+		var vm         = this;
+		vm.todoText    = '';
+		vm.listTitle   = 'TODOS Angular App';
+		vm.listItems   = [];
+		vm.addTodo     = addTodo;
+		vm.removeTodo  = removeTodo;
+		vm.changeState = changeState;
+
+		if ( !localStorage.getItem('todo-items' ) ) {
+			$http.get('js/list.json')
+				.success( function( data ){
+					setToLocalStorage( data );	
+
+					vm.listItems = getFromLocalStorage();
+				})
+				.error( function(){
+					alert('No TODO!');
+				});
+		} else {
+			vm.listItems = getFromLocalStorage();
+		}
+
+		function addTodo() {
+			var newTodo = {
+				text: vm.todoText,
+				isDone: false
+			};
+
+			vm.listItems.push(newTodo);
+			vm.todoText = ''
+
+			setToLocalStorage(vm.listItems);
+		};
+
+		function removeTodo( index ) {
+			vm.listItems = getFromLocalStorage();
+			vm.listItems.splice(index, 1);
+			
+			setToLocalStorage( vm.listItems );
+		}
+
+		function changeState( index ) {
+			vm.listItems = getFromLocalStorage();
+			var todo = vm.listItems[index];
+
+			todo.isDone = !todo.isDone;
+
+			setToLocalStorage( vm.listItems );
+		};
+
+		function getFromLocalStorage() {
+			return JSON.parse(localStorage.getItem('todo-items'));
+		};
+
+		function setToLocalStorage( data ) {
+			localStorage.setItem( 'todo-items', JSON.stringify( data ) );
+		};
+
+	}]);
+	
+})();
